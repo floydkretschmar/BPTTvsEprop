b7ed8bb15a0c1ef0272c88f865788662e17ef880
floydkretschmar <fkretschmar@googlemail.com>
2019-07-28 14:23:41 +0200

    optimized eprop func

--- src/eprop_func.py
+++ src/eprop_func.py
@@ -1,5 +1,6 @@
 import torch
 import torch.jit as jit
+import time
 
 
 class EProp1(torch.autograd.Function):
@@ -26,8 +27,6 @@ class EProp1(torch.autograd.Function):
         cellgate = torch.tanh(cellgate)
         outgate = torch.sigmoid(outgate)
 
-        gates = [ingate, forgetgate_y, cellgate]
-
         cy = (forgetgate_y * cx) + (ingate * cellgate)
         hy = outgate * torch.tanh(cy)
 
@@ -41,84 +40,88 @@ class EProp1(torch.autograd.Function):
         batch_size = input.size(0)
         input_size = input.size(1)
 
+        ingate = ingate.unsqueeze(2)
+        forgetgate_y = forgetgate_y.unsqueeze(2)
+        cellgate = cellgate.unsqueeze(2)
+        outgate = outgate.unsqueeze(2)
+        input = input.unsqueeze(2)
+        hx = hx.unsqueeze(2)
+
+        forgetgate_x = forgetgate_x.repeat(1, 3, 1)
+
         # the new eligibility vectors ...
-        ev_w_ih_y = torch.Tensor(ev_w_ih_x.size())
-        ev_w_hh_y = torch.Tensor(ev_w_hh_x.size())
-        ev_b_y = torch.Tensor(ev_b_x.size())
+        ev_w_ih_y = ev_w_ih_x.clone() * forgetgate_x
+        ev_w_hh_y = ev_w_hh_x.clone() * forgetgate_x
+        ev_b_y = ev_b_x.clone() * forgetgate_x
 
         # ... and eligibility traces
         et_w_ih_y = torch.Tensor(batch_size, 4 * hidden_size, input_size)
         et_w_hh_y = torch.Tensor(batch_size, 4 * hidden_size, hidden_size)
         et_b_y = torch.Tensor(batch_size, 4 * hidden_size, 1)
 
-        ev_w_ih_i, ev_w_ih_f, ev_w_ih_c = ev_w_ih_x.chunk(3, 1)
-        ev_w_hh_i, ev_w_hh_f, ev_w_hh_c = ev_w_hh_x.chunk(3, 1)
-        ev_b_i, ev_b_f, ev_b_c = ev_b_x.chunk(3, 1)
-
         ones = torch.ones(ingate.size())
+
+        ds_dw_ih = torch.Tensor(ev_w_ih_y.size())
+        ds_dw_hh = torch.Tensor(ev_w_hh_y.size())
+        ds_db = torch.Tensor(ev_b_y.size())
 
         # ingate
         base = ingate * (ones - ingate) * cellgate
-        #base = base
-        ds_dw_hh_i = base * hx
-        ds_dw_ih_i = base * input
-        ds_dbias_i = base
-        
-        ev_w_ih_y[:, :hidden_size, :] = forgetgate_x * ev_w_ih_i + ds_dw_hh_i.unsqueeze(2)
-        ev_w_hh_y[:, :hidden_size, :] = forgetgate_x * ev_w_hh_i + ds_dw_ih_i.unsqueeze(2)
-        ev_b_y[:, :hidden_size, :] = forgetgate_x * ev_b_i + ds_dbias_i.unsqueeze(2)
-        #print(ev_w_ih_y.shape)
+        ds_dw_hh[:, :hidden_size, :] = base * hx
+        ds_dw_ih[:, :hidden_size, :] = base * input
+        ds_db[:, :hidden_size, :] = base
         
         # forgetgate
         base = forgetgate_y * (ones - forgetgate_y) * cellgate
-        ds_dw_hh_f = base * hx
-        ds_dw_ih_f = base * input
-        ds_dbias_f = base
-        
-        ev_w_ih_y[:, hidden_size:(2 * hidden_size), :] = forgetgate_x * ev_w_ih_f + ds_dw_hh_f.unsqueeze(2)
-        ev_w_hh_y[:, hidden_size:(2 * hidden_size), :] = forgetgate_x * ev_w_hh_f + ds_dw_ih_f.unsqueeze(2)
-        ev_b_y[:, hidden_size:(2 * hidden_size), :] = forgetgate_x * ev_b_f + ds_dbias_f.unsqueeze(2)
-        #print(ev_w_ih_y.shape)
-        
+        ds_dw_hh[:, hidden_size:(2 * hidden_size), :] = base * hx
+        ds_dw_ih[:, hidden_size:(2 * hidden_size), :] = base * input
+        ds_db[:, hidden_size:(2 * hidden_size), :] = base
+
         # cellgate
         base = ingate * (ones - cellgate**2)
-        ds_dw_hh_c = base * hx
-        ds_dw_ih_c = base * input
-        ds_dbias_c = base
-        
-        ev_w_ih_y[:, (2 * hidden_size):(3 * hidden_size), :] = forgetgate_x * ev_w_ih_c + ds_dw_hh_c.unsqueeze(2)
-        ev_w_hh_y[:, (2 * hidden_size):(3 * hidden_size), :] = forgetgate_x * ev_w_hh_c + ds_dw_ih_c.unsqueeze(2)
-        ev_b_y[:, (2 * hidden_size):(3 * hidden_size), :] = forgetgate_x * ev_b_c + ds_dbias_c.unsqueeze(2)
-        #print(ev_w_ih_y.shape)
+        ds_dw_hh[:, (2 * hidden_size):(3 * hidden_size), :] = base * hx
+        ds_dw_ih[:, (2 * hidden_size):(3 * hidden_size), :] = base * input
+        ds_db[:, (2 * hidden_size):(3 * hidden_size), :] = base
+
+        ev_w_ih_y += ds_dw_ih
+        ev_w_hh_y += ds_dw_hh
+        ev_b_y += ds_db
 
         # calculate eligibility traces by multiplying the eligibility vectors with the outgate
-        for i in range(0, 3 * hidden_size, hidden_size):
-            et_w_ih_y[:, i:(i + hidden_size), :] = ev_w_ih_y[:, i:(i + hidden_size), :] * outgate.unsqueeze(2)
-            et_w_hh_y[:, i:(i + hidden_size), :] = ev_w_hh_y[:, i:(i + hidden_size), :] * outgate.unsqueeze(2)
-            et_b_y[:, i:(i + hidden_size), :] = ev_b_y[:, i:(i + hidden_size), :] * outgate.unsqueeze(2)
+        tmp_outgate = outgate.repeat(1, 3, 1)
+        et_w_ih_y[:, :3 * hidden_size, :] = ev_w_ih_y * tmp_outgate
+        et_w_hh_y[:, :3 * hidden_size, :] = ev_w_hh_y * tmp_outgate
+        et_b_y[:, :3 * hidden_size, :] = ev_b_y * tmp_outgate
         
         # The gradient of the output gate is only dependent on the observable state
         # => just use normal gradient calculation of dE/dh * dh/dweight 
         # => calculate second part of that equation now for input to hidden, hidden to hidden 
         #    and bias connections and multiply in the backward pass
-        base = outgate * (ones - outgate) * cy
-        et_w_ih_y[:, (3 * hidden_size):(4 * hidden_size)] = (base * hx).unsqueeze(2)
-        et_w_hh_y[:, (3 * hidden_size):(4 * hidden_size)] = (base * input).unsqueeze(2)
-        et_b_y[:, (3 * hidden_size):(4 * hidden_size)] = base.unsqueeze(2)
+        base = outgate * (ones - outgate) * cy.unsqueeze(2)
+        et_w_ih_y[:, (3 * hidden_size):(4 * hidden_size)] = base * hx
+        et_w_hh_y[:, (3 * hidden_size):(4 * hidden_size)] = base * input
+        et_b_y[:, (3 * hidden_size):(4 * hidden_size)] = base
 
-        ctx.save_for_backward(et_w_ih_y, et_w_hh_y, et_b_y)
+        ctx.intermediate_results = et_w_ih_y, et_w_hh_y, et_b_y
 
-        return hy, cy, ev_w_ih_y, ev_w_hh_y, ev_b_y, forgetgate_y.unsqueeze(2)
+        return hy, cy, ev_w_ih_y, ev_w_hh_y, ev_b_y, forgetgate_y
 
     @staticmethod
     # grad_ev_ih and grad_ev_hh should always be None
     def backward(ctx, grad_hy, grad_cy, grad_ev_w_ih, grad_ev_w_hh, grad_et_b, forgetgate_y):
-        et_w_ih_y, et_w_hh_y, et_b_y = ctx.saved_tensors
+        et_w_ih_y, et_w_hh_y, et_b_y = ctx.intermediate_results
 
-        hidden_size = int(et_w_ih_y.size(1) / 4)
-        grad_weight_ih = grad_weight_hh = grad_bias = None
+        tmp_grad_hy = grad_hy.unsqueeze(2).repeat(1, 4, 1)
+
+        #print(et_b_y.size(), tmp_grad_hy.size())
+
+        grad_weight_ih = et_w_ih_y.clone() * tmp_grad_hy
+        grad_weight_hh = et_w_hh_y.clone() * tmp_grad_hy
+        grad_bias = et_b_y.clone() * tmp_grad_hy
 
         # create tensors for the weight gradient
+        '''
+        hidden_size = int(et_w_ih_y.size(1) / 4)
         grad_weight_ih = torch.Tensor(et_w_ih_y.size())
         grad_weight_hh = torch.Tensor(et_w_hh_y.size())
         grad_bias = torch.Tensor(et_b_y.size())
@@ -132,7 +135,7 @@ class EProp1(torch.autograd.Function):
             grad_bias[:, i:i + hidden_size, :] = et_b_y[:, i:i + hidden_size, :] * grad_hy
 
         #print(grad_weight_ih.shape, grad_weight_hh.shape, grad_bias.shape)
-        grad_bias = grad_bias.squeeze()
+        grad_bias = grad_bias.squeeze()'''
 
         # grad_input, grad_ev_ih, grad_ev_hh, grad_hx, grad_cx, grad_weight_ih, grad_weight_hh, grad_bias_ih, grad_bias_hh
-        return None, None, None, None, None, None, None, grad_weight_ih, grad_weight_hh, grad_bias, grad_bias
+        return None, None, None, None, None, None, None, grad_weight_ih, grad_weight_hh, grad_bias.squeeze(), grad_bias.squeeze()
